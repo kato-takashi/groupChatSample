7a12cc9d07be32ba254409627c4b0f861e8755ce
Kato Takashi <taka4.kato@gmail.com>
2015-10-19 23:48:01 +0900

    sample

---
+++ HelloKii-Android/src/com/kii/world/HelloKii.java
@@ -0,0 +1,36 @@
+//
+//
+// Copyright 2012 Kii Corporation
+// http://kii.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+
+package com.kii.world;
+
+import android.app.Application;
+import com.kii.cloud.storage.Kii;
+import com.kii.cloud.storage.Kii.Site;
+
+public class HelloKii extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // initialize the Kii SDK!
+        Kii.initialize("a79404b1", "4e6710488c3cce6afe0165e197e8c866", Site.JP);
+    }
+
+}
